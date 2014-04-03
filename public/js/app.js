ed68c93e22425591441899f7a606ed4d2c6ce31a
Dario Fabbri <dar.fabbri@gmail.com>
2014-04-03 15:30:50 +0200

    Added routing and transitions.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 YUI.add('the-app', function(Y) {
 
 	Y.TheApp = Y.Base.create('theApp', Y.App, [], {
+
 		// Add or override prototype properties and methods here.
 		//
+
 		views: {
 			login: { type: 'LoginView' },
 			main: { type: 'MainView' }
@@ -12,15 +14,42 @@ YUI.add('the-app', function(Y) {
 
 			this.on('loginView:logon', function(e) {
 				Y.log(e);
-				this.showView('main');
+				this.navigate('/');
 			})
+		},
+
+		showLoginPage: function(req, res) {
+			this.showView('login');
+		},
+
+		showMainPage: function(req, res) {
+			this.showView('main');
 		}
 	}, {
 		// Add static properties and methods here.
 		//
+
+
 		ATTRS: {
 			// Add or override default attributes here.
 			//
+
+			root: {
+				value: '/'
+			},
+
+			routes: {
+				value: [
+					{ 
+						path: '/',
+						callbacks: 'showMainPage'
+					},
+					{ 
+						path: '/login', 
+						callbacks: 'showLoginPage'
+					}
+				]
+			}
 		}
 	});
 
